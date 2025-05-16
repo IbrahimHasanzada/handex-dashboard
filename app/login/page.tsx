ef72950b06d9f3c76a1e1b7ebcf350335beee7f6
IbrahimHasanzada <ibrahimhesenzade03@gmail.com>
2025-05-16 23:55:41 +0400

    cookie expires time updated

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -37,7 +37,7 @@ export default function LoginPage() {
             }).unwrap();
 
             Cookies.set('token', response.token, {
-                expires: 7,
+                expires: 1,
             });
             toast.success('Daxil olundu');
             router.push('/');
