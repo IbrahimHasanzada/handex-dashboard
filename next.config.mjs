002efd24a5a0e269afaf211fa3fa57582b95199f
IbrahimHasanzada <ibrahimhesenzade03@gmail.com>
2025-06-01 20:53:55 +0400

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,18 @@
-let userConfig = undefined
+let userConfig = undefined;
 try {
   // try to import ESM first
-  userConfig = await import('./v0-user-next.config.mjs')
+  userConfig = await import('./v0-user-next.config.mjs');
 } catch (e) {
   try {
     // fallback to CJS import
-    userConfig = await import("./v0-user-next.config");
+    userConfig = await import('./v0-user-next.config');
   } catch (innerError) {
     // ignore error
   }
 }
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const baseConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -26,32 +26,28 @@ const nextConfig = {
     webpackBuildWorker: true,
     parallelServerBuildTraces: true,
     parallelServerCompiles: true,
+    missingSuspenseWithCSRBailout: false,
   },
-}
+  transpilePackages: ['@tinymce/tinymce-react'],
+};
 
 if (userConfig) {
   // ESM imports will have a "default" property
-  const config = userConfig.default || userConfig
+  const config = userConfig.default || userConfig;
 
   for (const key in config) {
     if (
-      typeof nextConfig[key] === 'object' &&
-      !Array.isArray(nextConfig[key])
+      typeof baseConfig[key] === 'object' &&
+      !Array.isArray(baseConfig[key])
     ) {
-      nextConfig[key] = {
-        ...nextConfig[key],
+      baseConfig[key] = {
+        ...baseConfig[key],
         ...config[key],
-      }
+      };
     } else {
-      nextConfig[key] = config[key]
+      baseConfig[key] = config[key];
     }
   }
 }
 
-export default nextConfig
-
-// next.config.js
-module.exports = {
-  // TinyMCE kimi böyük kitabxanalar üçün transpile et
-  transpilePackages: ['@tinymce/tinymce-react'],
-};
+export default baseConfig;
