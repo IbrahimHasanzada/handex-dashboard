b2a28bc06e03181c66c3d06e9daab45b00cb1573
IbrahimHasanzada <ibrahimhesenzade03@gmail.com>
2025-06-01 21:01:40 +0400

    updated

--- components/blog/add-form/content-tab.tsx
+++ components/blog/add-form/content-tab.tsx
@@ -3,9 +3,16 @@
 import { FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-import { Editor } from "@tinymce/tinymce-react"
+const TinyMCE = dynamic(
+    () => import('@tinymce/tinymce-react').then((mod) => mod.Editor),
+    {
+        ssr: false, 
+        loading: () => <p>Editor yüklənir...</p>
+    }
+);
 import { editorConfig } from "@/utils/editor-config"
 import { placeholdersNews } from "@/utils/input-placeholders"
+import dynamic from "next/dynamic";
 import { useState } from "react"
 import type { UseFormReturn } from "react-hook-form"
 
@@ -53,7 +60,7 @@ export function ContentTab({ form, apiKey, selectedLanguage, setSelectedLanguage
                             <FormItem>
                                 <FormLabel>{lang === "az" ? "Məzmun" : lang === "en" ? "Content" : "Содержание"}</FormLabel>
                                 <FormControl>
-                                    <Editor
+                                    <TinyMCE
                                         value={field.value as string}
                                         onEditorChange={(content) => {
                                             field.onChange(content)
